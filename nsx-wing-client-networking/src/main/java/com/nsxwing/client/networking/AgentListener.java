9301232a750fa57464856152af532d6b99e8fdf1
Chris Greff <CGreff@Pillartechnology.com>
2016-02-08 21:16:59 -0500

    Shoring stuff up; adding dependency on common

--- nsx-wing-client-networking/src/main/java/com/nsxwing/client/networking/AgentListener.java
+++ nsx-wing-client-networking/src/main/java/com/nsxwing/client/networking/AgentListener.java
@@ -1,6 +1,7 @@
-package com.nsxwing.server.networking;
+package com.nsxwing.client.networking;
 
-import com.nsxwing.server.event.GameEvent;
+
+import com.nsxwing.common.event.GameEvent;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
