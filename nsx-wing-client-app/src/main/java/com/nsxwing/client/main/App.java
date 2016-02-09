89704c3e8111afdb24c2a02d17ad24486a4b708e
Chris Greff <CGreff@Pillartechnology.com>
2016-02-08 21:43:10 -0500

    A bit of spiking to make sure the server and client can talk to each other (they can).

--- nsx-wing-client-app/src/main/java/com/nsxwing/client/main/App.java
+++ nsx-wing-client-app/src/main/java/com/nsxwing/client/main/App.java
@@ -1,9 +1,27 @@
-package com.nsxwing.server.main;
+package com.nsxwing.client.main;
 
+import com.nsxwing.common.event.GameEvent;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+@Slf4j
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World.");
-    }
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("localhost", 5413);
+			ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+			while (true) {
+				log.info("Currently the input stream has : " + inputStream.available() + " available.");
+				GameEvent event = (GameEvent) inputStream.readObject();
+				log.info("Received event:" + event);
+				Thread.sleep(5000);
+			}
+		} catch (Exception e) {
+			log.debug("Something has gone awry.", e);
+		}
+	}
 
 }
